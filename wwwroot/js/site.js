c23c5a00155a2e5a0a8081b469393dd7402bd1af
charles1984s <charles@ether.com.tw>
2022-09-21 17:30:52 +0800

    [update] js檔案壓縮，增加banner輪播

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,24 @@
 ﻿function ready() {
     typeof (PageReady) === "function" && PageReady();
     $("#Collapse_Button > i").on("click", collapse);
+    $("#Floating_Objects").on("click", function () {
+        $('html,body').stop().animate({
+            scrollTop: 0
+        }, 0)
+    });
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 }
 
 function collapse() {
