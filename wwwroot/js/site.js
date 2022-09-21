e0e756a959655c60fdbb319b534aa650a4afb75f
charles1984s <charles@ether.com.tw>
2022-09-21 18:29:35 +0800

    [update] 增加cookies授權layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,8 @@
 ﻿function ready() {
     typeof (PageReady) === "function" && PageReady();
     $("#Collapse_Button > i").on("click", collapse);
+    $(".btn_cookie_accept").on("click", cookie_accept);
+    $(".btn_cookie_reject").on("click", cookie_reject);
     $("#Floating_Objects").on("click", function () {
         $('html,body').stop().animate({
             scrollTop: 0
@@ -23,4 +25,12 @@
 
 function collapse() {
     $("footer").toggleClass("footer_pack_up");
+}
+
+function cookie_accept() {
+    $("#Cookie").toggleClass("cookie_close");
+}
+
+function cookie_reject() {
+    $("#Cookie").toggleClass("cookie_close");
 }
