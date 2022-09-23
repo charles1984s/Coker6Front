8e4eefedea21378b01f792ff773e2193e8fbafed
charles1984s <charles@ether.com.tw>
2022-09-23 19:15:59 +0800

    [update] 首頁改版

---
+++ wwwroot/js/Footer/Footer.js
@@ -0,0 +1,11 @@
+﻿window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+        $(".btn_gotop").css('display', 'block');
+    } else {
+        $(".btn_gotop").css('display', 'none');
+    }
+}
---
+++ wwwroot/js/Header/Header.js
@@ -0,0 +1,11 @@
+﻿
+function PageReady() {
+    $(document).ready(function () {
+        setInterval(function () {
+            $('.news_box li:first-child').slideUp(function () {
+                $(this).appendTo($('.news_box')).slideDown()
+            })
+        }, 3000)
+    })
+}
+
