79bf9200197c8079e0453ce9904277359d19b261
charles1984s <charles@ether.com.tw>
2022-09-27 19:11:18 +0800

    [update] mega-menu調整，首頁CSS調整

--- wwwroot/js/Header/Header.js
+++ wwwroot/js/Header/Header.js
@@ -1,5 +1,12 @@
 ﻿function HeaderInit() {
-    console.log("in");
+    var $myOffcanvas = $("#Mega_Menu>.offcanvas");
+    $myOffcanvas.on('hidden.bs.offcanvas', function () {
+        $("#menuButton").addClass("collapsed");
+    });
+    $myOffcanvas.on('shown.bs.offcanvas', function () {
+        console.log($("#menuButton"));
+        $("#menuButton").removeClass("collapsed");
+    });
     setInterval(function () {
         $('.news_box li:first-child').slideUp(function () {
             $(this).appendTo($('.news_box')).slideDown()
--- wwwroot/js/Home/Index.js
+++ wwwroot/js/Home/Index.js
@@ -1,11 +1,56 @@
 ﻿
 function PageReady() {
     HeaderInit();
-    $(document).ready(function () {
-        $("#btn_gonews").on("click", gonews);
-    })
+    $("#btn_gonews").on("click", GoNews);
+    $(".btn_favorites").on("click", AddFavorites);
+
+    var banner_swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    var new_swiper = new Swiper(".NewsSwiper", {
+        slidesPerView: jQuery(window).width() > 768 ? 2: 1,
+        spaceBetween: 15,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 }
 
-function gonews() {
+function GoNews() {
     $('html, body').animate({ scrollTop: $("#News").offset().top }, 0);
+}
+
+function AddFavorites() {
+    var toastLiveExample = document.getElementById('liveToast')
+    $(this).toggleClass('fa-solid');
+    $("#liveToast>.toast-body").empty();
+    if ((this).classList.contains('fa-solid')) {
+        $("#liveToast>.toast-body").append('<div>加入收藏成功</div>');
+    } else {
+        $("#liveToast>.toast-body").append('<div>移除收藏成功</div>');
+    }
+    var toast = new bootstrap.Toast(toastLiveExample)
+    $('#Mask').toggleClass('show modal-backdrop');
+    toast.show()
+    setTimeout(function () {
+        toast.hide();
+        $('#Mask').toggleClass('show modal-backdrop');
+    }, 1500);
 }
