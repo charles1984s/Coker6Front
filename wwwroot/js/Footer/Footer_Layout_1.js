8e566d6f5f55bb8ccb6d7adbf38ee97f53b5d1de
charles1984s <charles@ether.com.tw>
2023-12-20 11:01:33 +0800

    [update] 版型五頁尾調整

--- wwwroot/js/Footer/Footer_Layout_1.js
+++ wwwroot/js/Footer/Footer_Layout_1.js
@@ -20,3 +20,18 @@ function MenuItemResize() {
         }
     })
 }
+
+function toggleFooterMenu() {
+    var footerMenu = document.getElementById('Footer_Menu');
+    var collapseButtonIcon = document.querySelector('#Collapse_Button > i');
+
+    if (footerMenu.style.height === '0px' || footerMenu.style.height === '') {
+        footerMenu.style.height = '210px';
+        collapseButtonIcon.style.transform = 'rotate(0deg)';
+        collapseButtonIcon.style.fontSize = '1.125rem';
+        collapseButtonIcon.style.transition = 'all 1s';
+    } else {
+        footerMenu.style.height = '0px';
+        collapseButtonIcon.style.transform = 'rotate(180deg)';
+    }
+}
