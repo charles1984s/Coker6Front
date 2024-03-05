94c0cb1bc7b959bd94a1fb59cfa383229d783359
charles1984s <charles@ether.com.tw>
2024-03-05 15:00:55 +0800

    [update] 商品列表微調

--- wwwroot/Shared/js/ViewTypeChange.js
+++ wwwroot/Shared/js/ViewTypeChange.js
@@ -10,49 +10,108 @@ const list_type = newEnum({
     grid: "Grid",
     list: "List",
     text: "Text",
+    product_grid: "Product_Grid",
 });
 
 const display_configurations = {
-    [list_type.list]: { ".box-shadow": "p-1 type3",
+    [list_type.list]: ["type1 bg-white", {
         ".card-border": "card-border-rd type3",
+        ".catalog-number":"d-none",
         ".check_btn":"check_btn-type3",
+        ".col": "p-1 type3 box-shadow",
         ".description": "type3-content",
         ".image":"px-0",
         ".image_frame":"h-100",
         ".imgh":"img-h",
+        ".item-header": "d-none",
+        ".like-and-share": "d-none",
         ".max-h":"max-hei",
         ".mergetag":"merge",
-        ".more-btn":"d-none",
+        ".more-btn": "d-none",
+        ".purchase": "d-none",
+        ".related-tag": "d-none",
+        ".tags": "d-none",
         ".title":"type1-title fw-bold p-2",
         "figcaption":"flex-grow-1 p-3 py-0 py-md-3 pb1",
-    },
-    [list_type.grid]: {
-        ".box-shadow": "p-1 type2",
+    }],
+    [list_type.grid]: ["type2 row row-cols-sm-4 bg-white", {
         ".card-border": "card-border-rd type2",
+        ".catalog-number": "d-none",
         ".check_btn": "w-100",
+        ".col": "p-1 type2 box-shadow",
         ".date": "d-none",
         ".description": "d-none type2-content",
         ".image_frame": "w-100",
+        ".item-header": "d-none",
+        ".like-and-share": "d-none",
         ".more": "d-none",
         ".more-btn": "d-none",
+        ".purchase": "d-none",
+        ".related-tag": "d-none",
+        ".tags": "d-none",
         ".title": "type1-title fw-bold p-2",
         "figcaption": "pb1",
         "figure": "flex-column",
-    },
-    [list_type.text]: {
-        ".box-shadow": "p-1 type1 text-border",
+    }],
+    [list_type.text]: ["type3 row row-cols-sm-4 bg-white", {
         ".card-border": "pr3 type1",
+        ".catalog-number": "d-none",
         ".check_btn": "w-100",
+        ".col": "p-1 type1 text-border box-shadow",
         ".date": "d-none",
         ".description": "d-none",
         ".image": "d-none",
         ".image_frame": "d-none",
+        ".item-header": "d-none",
+        ".like-and-share": "d-none",
         ".more": "d-none",
+        ".purchase": "d-none",
         ".related-tag": "d-none",
         ".search-more": "d-none",
+        ".tags": "d-none",
         ".title": "type2-title pr3 h-100",
         "figure": "flex-column",
-    },
+    }],
+    [list_type.product_grid]: ["type4 row row-cols-lg-4 bg-light px-2", {
+        ".card-border": "card-border-rd type2",
+        ".catalog-number": "type4-title d-inline",
+        ".check_btn": "w-100",
+        ".col": "p-1 type4 rounded-lg h-100",
+        ".date": "d-none",
+        ".description": "d-none type2-content",
+        ".image_frame": "type4-image-frame w-100",
+        ".item-header": "d-flex",
+        ".like-and-share": "d-none",
+        ".more": "d-none",
+        ".more-btn": "d-none",
+        ".purchase":"d-none",
+        ".tags": "mt-auto align-text-bottom",
+        ".title": "type4-title d-inline fs-6",
+        "figcaption": "pb1 type4-caption d-flex flex-column",
+        "figure": "flex-column",
+    }],
+    [list_type.product_list]: ["type1 bg-light px-2", {
+        ".card-border": "card-border-rd type2",
+        ".catalog-number": "type4-title d-inline",
+        ".check_btn": "check_btn-type3",
+        ".col": " type5 rounded-lg",
+        ".description": "d-none",
+        ".image": "px-0",
+        ".image_frame": "h-100 w-25",
+        ".imgh": "img-h",
+        ".item-header": "d-none",
+        ".item-title": "d-flex",
+        ".like-and-share":"d-inline ms-auto fs-5 p-2",
+        ".max-h": "max-hei",
+        ".mergetag": "merge",
+        ".more":"d-none",
+        ".more-btn": "d-none",
+        ".purchase": "ms-auto d-inline",
+        ".related-tag": "d-none",
+        ".tags": "mt-auto mb-2 align-text-bottom d-flex",
+        ".title": "type4-title d-inline fs-6 p-2",
+        "figcaption": "flex-grow-1 d-flex flex-column",
+    }],
 }
 
 function ViewTypeChangeInit() {
@@ -66,13 +125,13 @@ function ViewTypeChangeInit() {
 
             $btn_grid.on("click", function () {
                 if (!$btn_grid.data("activate")) {
-                    typeChange($btn_grid, $btn_list, $btn_text, $content, list_type.grid);
+                    typeChange($btn_grid, $btn_list, $btn_text, $content, list_type.product_grid);
                 }
             })
 
             $btn_list.on("click", function () {
                 if (!$btn_list.data("activate")) {
-                    typeChange($btn_list, $btn_grid,  $btn_text , $content, list_type.list);
+                    typeChange($btn_list, $btn_grid,  $btn_text , $content, list_type.product_list);
                 }
             })
 
@@ -92,11 +151,13 @@ function ViewTypeChangeInit() {
 
 function updateStyle($self, configuration) {
     for (const [_, config] of Object.entries(display_configurations)) {
-        for (const [selector, classes] of Object.entries(config)) {
+        $self.removeClass(config[0]);
+        for (const [selector, classes] of Object.entries(config[1])) {
             $self.find(selector).removeClass(classes);
         }
     }
-    for (const [selector, classes] of Object.entries(configuration)) {
+    $self.addClass(configuration[0]);
+    for (const [selector, classes] of Object.entries(configuration[1])) {
         $self.find(selector).addClass(classes);
     }
 }
@@ -107,33 +168,8 @@ function typeChange($self, $brother ,$brother2, $content, type) {
     $brother.addClass("text-black-50");
     $brother2.data("activate", 0);
     $brother2.addClass("text-black-50");
-    switch (type) {
-        case list_type.list:
-            $content.each(function () {
-                var $self = $(this)
-                $self.addClass("type3").removeClass("type1 type2");
-                $self.removeClass("row row-cols-sm-4");
-                /*$self.removeClass("row row-cols-2 row-cols-sm-4 row-cols-lg-6");*/
-                updateStyle($self, display_configurations[list_type.list])
-                
-            })
-            break;
-        case list_type.grid:
-            $content.each(function () {
-                var $self = $(this)
-                $self.addClass("type2").removeClass("type1 type3");
-                $self.addClass("row row-cols-sm-4");
-                /* $self.addClass("row row-cols-2 row-cols-sm-4 row-cols-lg-6");*/
-                updateStyle($self, display_configurations[list_type.grid])
-            });
-            break;
-        case list_type.text:
-            $content.each(function () {
-                var $self = $(this)
-                $self.addClass("type1").removeClass("type2 type3");
-                $self.addClass("row row-cols-sm-4");
-                updateStyle($self, display_configurations[list_type.text])
-            });
-            break;
-    }
+    $content.each(function () {
+        var $self = $(this)
+        updateStyle($self, display_configurations[type])
+    })
 }
