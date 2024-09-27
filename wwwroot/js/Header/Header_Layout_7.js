925c8c2153587647d629d14a3b2273108a19c6a6
charles1984s <charles@ether.com.tw>
2024-09-27 12:59:07 +0800

    [update] 廣告人氣值排序規則調整、版型七跑馬燈不同裝置校正

--- wwwroot/js/Header/Header_Layout_7.js
+++ wwwroot/js/Header/Header_Layout_7.js
@@ -5,7 +5,30 @@ function HeaderInit() {
     $("#breadcrumb").prependTo($main);
     $contain.appendTo($main);
     $("#main").append($main);
-
+    $(".marqueeSwiper").each(function () {
+        const $marquee = $(this).find(".swiper-wrapper"); 
+        console.log("網站的開設讓大眾能方便的瞭解與本公司相關的資訊，歡迎有需要買賣剪床、折床的客戶，".length);
+        $marquee.find(".swiper-slide").each(function () {
+            let maxLen = 100;
+            let $slide = $(this);
+            let txt = $slide.text().replace("(current)", "");
+            if ($(window).width() < 576) { 
+                maxLen = 15;
+            } else if ($(window).width() < 768) {
+                maxLen = 21;
+            } else if ($(window).width() < 992) {
+                maxLen = 38;
+            }
+            const count = Math.floor(txt.length / maxLen) - (txt.length % maxLen > 0 ? 0 : 1);
+            if (count > 0) $slide.find(".text").text(txt.substring(0, maxLen));
+            for (let i = 1; i < count; i++) {
+                let $newSlide = $slide.clone();
+                $newSlide.find(".text").text(txt.substring((i * maxLen), ((i+1)* maxLen)));
+                console.log($newSlide.find(".text").text(), i , maxLen);
+                $slide.after($newSlide);
+            }
+        });
+    });
     var marqueeSwiper = new Swiper(".marqueeSwiper", {
         direction: "vertical",
         allowTouchMove: false,
