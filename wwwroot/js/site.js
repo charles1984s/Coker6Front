6e5f44496ed0b62bab1ca0bfa4075002279fdc69
charles1984s <charles@ether.com.tw>
2024-10-16 11:12:20 +0800

    [update] 版型六畫面微調

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -174,8 +174,6 @@ function ready() {
             });
         }
     };
-
-    $.cookie('Member_Name', "會員一", { path: '/' });
     typeof (PageReady) === "function" && PageReady();
     typeof (HeaderInit) === "function" && HeaderInit();
     typeof (FooterInit) === "function" && FooterInit();
