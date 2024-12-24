1271c771ae4aad675fa96bc4a7028da7f1ffaad7
charles1984s <charles@ether.com.tw>
2024-12-24 13:32:27 +0800

    [update] 調整無障礙 focus 時輪播元件不會自動索引問題v1.1

--- wwwroot/Shared/js/Swiper.js
+++ wwwroot/Shared/js/Swiper.js
@@ -104,7 +104,8 @@ function SwiperInit(obj) {
             thisSwiper = $(this);
             $(this).off("mouseover").on("mouseover", stop);
             $(this).find(".swiper-slide a,.swiper-slide").off("focus").on("focus", function () {
-                const activeIndex = $(swiper.el).find(":focus").parents(".swiper-slide").attr("aria-label").split(" / ")[0];
+                const $item = $(this).hasClass("swiper-slide") ? $(this) : $(this).parents(".swiper-slide");
+                const activeIndex = $item.attr("aria-label").split(" / ")[0];
                 swiper.slideTo(activeIndex - 1, 300);
                 stop();
             });
